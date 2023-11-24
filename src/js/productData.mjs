899f051b570c1eab966d7b0952fee6cdc68ac436
ibukunoluwashina <ibukunoluwashina@gmail.com>
2023-11-24 16:13:29 +0100

    wdd330project

--- src/js/productData.mjs
+++ src/js/productData.mjs
@@ -1,18 +1,29 @@
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
-}
+import { findProductById } from "./productData.mjs";
+import { setLocalStorage } from "./utils.mjs";
 
-export function getData(category = "tents") {
-  return fetch(`../json/${category}.json`)
-    .then(convertToJson)
-    .then((data) => data);
-}
+let product = {};
 
-export async function findProductById(id) {
-  const products = await getData();
-  return products.find((item) => item.Id === id);
+export default async function productDetails(productId) {
+  // get the details for the current product. findProductById will return a promise! use await or .then() to process it
+  product = await findProductById(productId);
+  // once we have the product details we can render out the HTML
+  renderProductDetails();
+  // once the HTML is rendered we can add a listener to Add to Cart button
+  document.getElementById("addToCart").addEventListener("click", addToCart);
+}
+function addToCart() {
+  setLocalStorage("so-cart", product);
+}
+function renderProductDetails() {
+  document.querySelector("#productName").innerText = product.Brand.Name;
+  document.querySelector("#productNameWithoutBrand").innerText =
+    product.NameWithoutBrand;
+  document.querySelector("#productImage").src = product.Image;
+  document.querySelector("#productImage").alt = product.Name;
+  document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+  document.querySelector("#productColorName").innerText =
+    product.Colors[0].ColorName;
+  document.querySelector("#productDescriptionHtmlSimple").innerHTML =
+    product.DescriptionHtmlSimple;
+  document.querySelector("#addToCart").dataset.id = product.Id;
 }
